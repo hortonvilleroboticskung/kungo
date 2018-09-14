ac66a43e67ebb35fa8f5dc25433737662c20c5d1
Kung Lee <kung@hortonvillerobotics.com>
2018-09-13 22:12:47 -0500

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import org.json.*;
+
+import java.io.*;
+//import org.json.simple.JSONArray;
+//import org.json.simple.JSONObject;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        File data = new File("C:\\Users\\heuve\\Desktop\\file.txt");
+        String dataString = "";
+        try {
+            if(!data.exists()){
+                data.createNewFile();
+            }
+            InputStream i = new FileInputStream(data);
+            byte[] in = new byte[(int)data.length()];
+            i.read(in);
+            dataString = new String(in);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        JSONObject trial = new JSONObject(dataString);
+        System.out.println(trial.getJSONObject("name").get("first"));
+        System.out.println(trial.getJSONObject("actions").get("id"));
+//l
+
+
+
+    }
+}
