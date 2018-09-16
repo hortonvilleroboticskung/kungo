4604cdf106531690f9553dcdd7f8353c1827d4b8
Kung Lee <kung@hortonvillerobotics.com>
2018-09-16 15:14:43 -0500

    End of Sunday

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,31 @@
 import org.json.*;
-
+import java.util.*;
 import java.io.*;
+import java.lang.reflect.Array;
 //import org.json.simple.JSONArray;
 //import org.json.simple.JSONObject;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-            FileInputStream fstream = new FileInputStream("C:\\Users\\kungl\\Desktop\\jfile.json");
+        FileInputStream fstream = new FileInputStream("C:\\Users\\kungl\\Desktop\\jfile.json");
         BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
         JSONObject trial = new JSONObject(br.readLine());
-        System.out.println(trial.getString("id"));
+        JSONArray actions = trial.getJSONArray("actions");
+        String boardName = "";
+        String boardId = "";
+        String listName = "";
+        String listId = "";
+        ArrayList<String> cardName = new ArrayList<>();
+
+        listName = actions.getJSONObject(1).getJSONObject("data").getJSONObject("list").getString("name");
+        listId = actions.getJSONObject(1).getJSONObject("data").getJSONObject("list").getString("id");
+        boardName = actions.getJSONObject(1).getJSONObject("data").getJSONObject("board").getString("name");
+
+          System.out.println(listId);
+          System.out.println(boardName);
+          System.out.println(listName);
+}
 
 
 
@@ -20,4 +35,4 @@ public class Main {
 
 
     }
-}
+
