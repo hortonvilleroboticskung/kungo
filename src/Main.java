e071dbd7a334f56480150af6388f6b18cacf0926
Ben Nowotny <ben@hortonvillerobotics.com>
2018-09-19 07:05:15 -0500

    Added comments in Main to detail work to be done.

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.lang.reflect.Array;
 //import org.json.simple.JSONObject;
 
 public class Main {
-//.has
+    //.has
     public static void main(String[] args) throws IOException{
 
         FileInputStream fstream = new FileInputStream("C:\\Users\\kungl\\Desktop\\jfile.json");
@@ -14,12 +14,21 @@ public class Main {
         JSONObject jsonfile = new JSONObject(br.readLine());
         JSONArray actions = jsonfile.getJSONArray("actions");
         ArrayList<Card> cards = new ArrayList<>();
+        HashMap<String, Integer> idToIndex= new HashMap<>();
 
         for(int i = actions.length()-1; i >= 0; i--) {
             JSONObject obj = actions.getJSONObject(i);
-            //TODO:INSERT CODE HERE
+            //Example (Adding card)
+            if(obj.has("card")) {
+                if (obj.getString("type").equals("createCard")) {
+                    idToIndex.put(obj.getJSONObject("card").getString("id"), cards.size()-1);
+                    cards.add(new Card(obj.getString("date"), obj.getJSONObject("card").getString("id"), obj.getJSONObject("card").getString("name"), obj.getJSONObject("list").getString("name")));
+                } else if(obj.getString("type").equals("INSERT TYPE HERE")) {
+                    Card currentCard = cards.get(idToIndex.get(obj.getJSONObject("card").getString("id")));
+                    //ADD CHANGES TO CURRENTCARD
+                }
+            }
         }
     }
-
 }
 
