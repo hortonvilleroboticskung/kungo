cc26b0fb6a2d745b18ead8d9bbdd0e98ecc8f2c4
Ben Nowotny <ben@hortonvillerobotics.com>
2018-09-19 00:47:39 -0500

    Card Class brought to spec. Comment added for code entry in Main class.

--- src/Card.java
+++ src/Card.java
@@ -4,34 +4,66 @@ import java.util.*;
 
 public class Card {
 
-    String id;
-    String name;
-    String currentList;
-    private ArrayList<String> comments;
+    private String id;
+    private String name;
+    private String currentList;
+    private HashMap<String, String> comments;
     private ArrayList<String> members;
     private ArrayList<String> labels;
-    public final String kungId = "5b71c696311f123b5bf6db7d";
+    private ArrayList<String> masterRecord;
 
-    public Card(String id, String name, String currentList) {
+    public Card(String timeStamp, String id, String name, String currentList) {
+        masterRecord = new ArrayList<>();
+        addToRecord(timeStamp,"Task created under \""+currentList+"\" list.");
         this.id = id;
         this.name = name;
         this.currentList = currentList;
-        comments = new ArrayList<>();
+        comments = new HashMap<>();
         members = new ArrayList<>();
         labels = new ArrayList<>();
-    }
-
-    public void addMember(Card card, String memberId) {
-
-        card.members.add(memberId);
 
     }
 
-    public void showMembers(Card card) {
-
+    public void addMember(String timeStamp, String memberName) {
+        addToRecord(timeStamp,""+formatName(memberName)+" added to task.");
+        members.add(memberName);
     }
-//    public static void main(String[] args){
-//        Card a = new Card("a","a","a");
-//    }
+
+    public void changeList(String timeStamp, String newList){
+        addToRecord(timeStamp,"Moved from \""+currentList+"\" to \""+newList+"\".");
+        currentList = newList;
+    }
+
+    public void addComment(String timeStamp, String memberName, String content){
+        addToRecord(timeStamp,formatName(memberName)+" commented \""+content+"\".");
+        comments.put(memberName,content);
+    }
+
+    public void changeLabels(String timeStamp, String[] labelList){
+        String listOfLabels = labelList[0];
+        for(int i = 1; i < labelList.length; i++) listOfLabels +=", "+labelList[i];
+        addToRecord(timeStamp, "Tagged as "+listOfLabels+".");
+        labels.clear();
+        for(String s : labelList) labels.add(s);
+    }
+
+    public String getId(){return id;}
+
+    private void addToRecord(String timeStamp, String contents){
+        masterRecord.add(formatDate(timeStamp)+":"+contents);
+    }
+
+    private String formatName(String name){
+        return name.substring(0,1).toUpperCase()+name.substring(1);
+    }
+
+    private String formatDate(String timeStamp){
+        return timeStamp.split("T")[0];
+    }
+
+    @Override
+    public boolean equals(Object o){
+        return o instanceof Card ? ((Card) o).getId().equals(getId()) : o instanceof String && o.equals(getId());
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -15,25 +15,10 @@ public class Main {
         JSONArray actions = jsonfile.getJSONArray("actions");
         ArrayList<Card> cards = new ArrayList<>();
 
-        for(int i = actions.length()-1; i >= 0; i--){
-            if(actions.getJSONObject(i).getString("type").equals("createCard")){
-                Card newCard = new Card(
-                        actions.getJSONObject(i).getJSONObject("data").getJSONObject("card").getString("id"),
-                        actions.getJSONObject(i).getJSONObject("data").getJSONObject("card").getString("name"),
-                        actions.getJSONObject(i).getJSONObject("data").getJSONObject("list").getString("name")
-                );
-                cards.add(newCard);
-                //System.out.println(newCard.id + "\n" + newCard.name + "\n" + newCard.currentList);
-            }
-            if(actions.getJSONObject(i).getString("type").equals("addMemberToCard") && cards.size()!=0){
-                for(int ii = 0; ii < cards.size(); ii++){
-                    if(cards.get(ii).id.equals(actions.getJSONObject(i).getJSONObject("data").getJSONObject("card").getString("id"))){
-                        cards.get(ii).addMember(cards.get(ii),actions.getJSONObject(i).getJSONObject("member").getString("fullName"));//TODO:Can i get it so i can just use the object as a parameter?
-                    }
-                }
-            }
+        for(int i = actions.length()-1; i >= 0; i--) {
+            JSONObject obj = actions.getJSONObject(i);
+            //TODO:INSERT CODE HERE
         }
-
     }
 
 }
