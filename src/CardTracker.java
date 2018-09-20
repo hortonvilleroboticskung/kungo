f9cbaf3a2f302b8284b3d7833b91d1394ab4f7ac
hortonvilleroboticsgrace <41523342+hortonvilleroboticsgrace@users.noreply.github.com>
2018-09-20 16:09:13 -0500

    CHANGE

---
+++ src/CardTracker.java
@@ -0,0 +1,23 @@
+public class CardTracker {
+
+    String[] listPosition;
+    String[] listDates;
+    String name;
+    String id;
+    String comment;
+    String description;
+    String members;
+
+
+    public CardTracker(String[] listPosition, String[] listDates, String name, String id, String comment, String description, String members){
+
+
+
+
+
+
+
+
+
+    }
+}
